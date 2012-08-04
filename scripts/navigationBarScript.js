e9139a81e6afa5c831d877fcfc0b16fd807c83ca
Alex Jackson <alex@alexj.me>
2012-08-04 12:33:36 +0100

    em based site header and alignments in the site header.

--- scripts/navigationBarScript.js
+++ scripts/navigationBarScript.js
@@ -1,12 +1,12 @@
 $(window).scroll(function () {
 	var pageFooter = $('.pageFooter');
 	if ($(window).scrollTop() + $(window).height() === $(document).height()) {
-		pageFooter[0].style.marginTop = "-50px";
-		pageFooter[0].style.height = "50px";
+		pageFooter[0].style.marginTop = "-3.3em";
+		pageFooter[0].style.height = "3.3em";
 		pageFooter[0].style.opacity = "0.9";
 	}
 	if ($(window).scrollTop() + $(window).height() !== $(document).height()) {
-		if (pageFooter[0].style.marginTop === "-50px") {
+		if (pageFooter[0].style.marginTop === "-3.3em") {
 			pageFooter[0].style.cssText = "";
 		}
 	}
