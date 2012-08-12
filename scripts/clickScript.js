8cdd5f3fb73d2e01d46d348d46306d63c8196ac5
Alex Jackson <alex@alexj.me>
2012-08-12 14:14:49 +0100

    Syntax improvements to clickScript.

--- scripts/clickScript.js
+++ scripts/clickScript.js
@@ -1,29 +1,24 @@
-function expandDiv(index) {
+function expandDiv(index){
     var eles = $(".unit-div");
     var unitDiv = eles[index];
-    var originalHeight = "3em";
-    var expandedHeight = "23.3em";
-    
-    
-    /* The first if statement is a hack to get styling to work. Since JQuery applies the styles to an element's style attribute and I don't do that when building websites, I need to check for the first time a div is clicked, otherwise nothing will happen when it's clicked. There's probably a really easy way to fix this but CBA reading the JQuery docs. Honestly, I wish I could get rid of JQuery, but IE doesn't have a getElementsByClassName method, so I can't. */
-    
-    if (unitDiv.style.height === originalHeight || unitDiv.style.height === "") {
+    var originalHeight = "3em", expandedHeight = "23.3em";
+
+    if (unitDiv.style.height === originalHeight || unitDiv.style.height === ""){
         unitDiv.style.height = expandedHeight;
         unitDiv.style.overflowX = "auto";
         unitDiv.style.overflowY = "auto";
         unitDiv.style.overflow = "auto";
-    } else if (unitDiv.style.height !== originalHeight) {
+    } else if (unitDiv.style.height !== originalHeight){
         unitDiv.style.height = originalHeight;
         unitDiv.scrollTop = 0;
         unitDiv.style.overflowX = "hidden";
         unitDiv.style.overflowY = "hidden";
         unitDiv.style.overflow = "hidden";
     }
-    
-    var i = 0;
-    while (i < eles.length) {
-        if (i !== index) {
-            if (eles[i].style.height !== originalHeight) {
+
+    for (var i = 0; i < eles.length; i++){
+        if (i !== index){
+                if (eles[i].style.height !== originalHeight){
                 eles[i].style.height = originalHeight;
                 eles[i].scrollTop = 0;
                 eles[i].style.overflowX = "hidden";
@@ -31,6 +26,5 @@ function expandDiv(index) {
                 eles[i].style.overflow = "hidden";
             }
         }
-        i++;
     }
 }
